2fff84e667b8640fe926afe6aa5e84665f059712
visbm <visbm@mail.ru>
2022-02-16 15:00:21 +0300

    Added base

---
+++ hotel/internal/handlers/roomHandlers/deleteRooms.go
@@ -0,0 +1,43 @@
+package roomhandlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"hotel/hotel/internal/store"
+	"hotel/hotel/pkg/response"
+	"log"
+
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// DeleteRooms ...
+func DeleteRooms(s *store.Store) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+		id, err := strconv.Atoi(ps.ByName("id"))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			s.Logger.Errorf("Bad request. Err msg:%v. Requests body: %v", err, r.FormValue("id"))
+			return
+		}
+		err = s.Open()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			s.Logger.Errorf("Can't open DB. Err msg:%v.", err)
+			return
+		}
+		err = s.Room().Delete(id)
+		if err != nil {
+			log.Print(err)
+			s.Logger.Errorf("Can't delete room. Err msg:%v.", err)
+			return
+		}
+		s.Logger.Info("Delete room with id = %d", id)
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(response.Info{Messsage: fmt.Sprintf("Delete room with id = %d", id)})
+
+	}
+}
---
+++ hotel/internal/store/store.go
@@ -0,0 +1,92 @@
+package store
+
+import (
+	"database/sql"
+	"fmt"
+	"hotel/hotel/internal/config"
+	"hotel/hotel/pkg/logging"
+
+	_ "github.com/lib/pq" // ...
+)
+
+// Store ...
+type Store struct {
+	Config          *config.Config
+	Db              *sql.DB
+	Logger          *logging.Logger
+	HotelRepository *HotelRepository
+	RoomRepository  *RoomRepository
+	SeatRepository  *SeatRepository
+}
+
+// New ...
+func New(config *config.Config) *Store {
+	return &Store{
+		Config: config,
+		Logger: logging.GetLogger(),
+	}
+}
+
+// Open ...
+func (s *Store) Open() error {
+	dataSourceName := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		s.Config.DataBase.Host,
+		s.Config.DataBase.Port,
+		s.Config.DataBase.Username,
+		s.Config.DataBase.Password,
+		s.Config.DataBase.DbName,
+		s.Config.DataBase.Sslmode,
+	)
+
+	db, err := sql.Open("postgres", dataSourceName)
+	if err != nil {
+		return err
+	}
+
+	if err := db.Ping(); err != nil {
+		return err
+	}
+
+	s.Db = db
+
+	return nil
+}
+
+// Close ...
+func (s *Store) Close() {
+	s.Db.Close()
+}
+
+// Hotel ...
+func (s *Store) Hotel() *HotelRepository {
+	if s.HotelRepository != nil {
+		return s.HotelRepository
+	}
+	s.HotelRepository = &HotelRepository{
+		Store: s,
+	}
+	return s.HotelRepository
+}
+
+// Room ...
+func (s *Store) Room() *RoomRepository {
+	if s.RoomRepository != nil {
+		return s.RoomRepository
+	}
+	s.RoomRepository = &RoomRepository{
+		Store: s,
+	}
+	return s.RoomRepository
+}
+
+// Seat ...
+func (s *Store) Seat() *SeatRepository {
+	if s.SeatRepository != nil {
+		return s.SeatRepository
+	}
+	s.SeatRepository = &SeatRepository{
+		Store: s,
+	}
+	return s.SeatRepository
+}
